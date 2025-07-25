c7a57bd654e2220d023293423e7f931ec888da96
Amarnath Sahni <144561044+Amarnath-sahni@users.noreply.github.com>
2025-07-25 20:56:16 +0530

    Time: 79 ms (5.17%) | Memory: 44.2 MB (5.40%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -2,19 +2,20 @@ class Solution {
     // Function to check if placing a Queen is safe
     public static boolean isSafe(char[][] board, int row, int col){
         // Check same column upwards
-        for (int i = row - 1; i >= 0; i--) {
-            if (board[i][col] == 'Q') return false;
-        }
+       for(int i=row-1; i>=0; i--){ //no need to move colum
+        if(board[i][col]=='Q') return false;
+       }
 
         // Check upper left diagonal
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j]=='Q') return false;
         }
 
         // ✅ Correct: check upper **right** diagonal
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') return false;
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j]=='Q') return false;
         }
+        
 
         return true;
     }
@@ -42,6 +43,7 @@ class Solution {
 
     // Helper to print board (optional)
     public static void printqueen(char[][] board) {
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j] + " ");
