156727ec1e7b4b7d5aaa3013561805c0e8481cc9
Amarnath Sahni <144561044+Amarnath-sahni@users.noreply.github.com>
2025-09-01 20:34:58 +0530

    Time: 14 ms (88.33%) | Memory: 43.7 MB (99.71%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,27 +1,21 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
         int max = amount+1;
 
-        int[][] dp = new int[n + 1][amount + 1];
-
-        // initialize
-        for (int j = 1; j <= amount; j++) {
-            dp[0][j] = max; // 0 coins can't make positive amount
+        int dp[] = new int[amount+1];
+        for(int i=0; i<=amount; i++){
+            dp[i] = max;
         }
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 0; // to make amount 0, need 0 coins
-        }
+        dp[0] = 0;
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=amount; j++){
-                if(coins[i-1] <= j){
-                    dp[i][j] = Math.min(dp[i - 1][j], 1 + dp[i][j-coins[i - 1]]);
-                }else{
-                     dp[i][j] = dp[i-1][j];
+        for(int i =1; i<=amount; i++){
+            for(int coin : coins){
+                if(i-coin >= 0){
+                    dp[i] = Math.min(dp[i], dp[i-coin]+1); 
                 }
             }
         }
-        return dp[n][amount] > amount ? -1 :dp[n][amount];
+
+        return dp[amount]> amount ? -1 : dp[amount];
     }
 }
