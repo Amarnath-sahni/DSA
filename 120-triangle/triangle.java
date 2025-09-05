c2d19a2cd572c9df694ffb35163d416544d57ebe
Amarnath Sahni <144561044+Amarnath-sahni@users.noreply.github.com>
2025-09-05 19:00:09 +0530

    Time: 4 ms (45.04%) | Memory: 45.5 MB (33.74%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -4,9 +4,9 @@ class Solution {
         int[] dp = new int[n + 1]; // extra space for easier calculation
 
         // Start from the bottom row and move upward
-        for (int row = n - 1; row >= 0; row--) {
-            for (int col = 0; col < triangle.get(row).size(); col++) {
-                dp[col] = Math.min(dp[col], dp[col + 1]) + triangle.get(row).get(col);
+        for(int i=n-1; i>=0; i--){
+            for(int col=0; col<triangle.get(i).size(); col++){
+                dp[col] = Math.min(dp[col], dp[col+1]) + triangle.get(i).get(col);
             }
         }
         return dp[0]; // answer at the top
