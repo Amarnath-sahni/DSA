0dddb1469d37ae3179ad1c59e6e5ffddf6daacde
Amarnath Sahni <144561044+Amarnath-sahni@users.noreply.github.com>
2026-02-15 16:19:10 +0530

    Time: 4 ms (93.70%) | Memory: 44.2 MB (87.24%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,34 +1,31 @@
 class Solution {
     public String reverseWords(String s) {
-       StringBuilder sb = new StringBuilder("");
-      StringBuilder word = new StringBuilder("");
+    StringBuilder sb = new StringBuilder("");
 
-      for(int i=s.length()-1; i>=0; i--){
-        char ch = s.charAt(i);
+    int i =  s.length()-1;
+    while(i >= 0){
 
-if(ch == ' ' && sb.length() == 0 && word.length() == 0){
-    continue;
-}
-        if(ch != ' '){
-            word.insert(0, ch); // 0th e , ue, lue, blue
-        }else if(word.length() > 0){
-            if(sb.length() > 0 ){
-                sb.append(' ');
-            }
-
-            sb.append(word);
-            word.setLength(0); // Clear the String
+        //if space from right side then skip
+        while(i >= 0 && s.charAt(i) == ' '){
+            i--;
         }
-      }
+        if(i<0){
+            break;
+        }
 
-      //append last world if String don't end with empty string
-      if(word.length() > 0){
+        //ending of word
+        int j = i;
+
+        while(i >=0 && s.charAt(i) != ' '){
+            i--;
+        }
+
         if(sb.length() > 0){
-            sb.append(" ");
+            sb.append(' '); 
         }
 
-        sb.append(word);
-      }
+        sb.append(s.substring(i+1, j+1));
+    }
 
       return sb.toString();
     }
