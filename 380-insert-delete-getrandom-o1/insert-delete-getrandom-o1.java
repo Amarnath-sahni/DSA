8556731d64ba7b665ef17d8e706edcf7dd7fa6e2
Amarnath Sahni <144561044+Amarnath-sahni@users.noreply.github.com>
2026-07-21 21:44:43 +0530

    Time: 29 ms (35.45%) | Memory: 100.7 MB (38.57%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,41 +1,53 @@
 class RandomizedSet {
-    HashSet<Integer> set;
+    List<Integer>list;
+    HashMap<Integer, Integer>map;
     Random random;
 
     public RandomizedSet() {
-       set = new HashSet<>();
-        random = new Random(); 
-        
+       map = new HashMap<>();
+       list = new ArrayList<>(); 
+        random = new Random();
     }
     
     public boolean insert(int val) {
-         if (set.contains(val)) {
+         if (map.containsKey(val)) {
             return false;
         }
-        set.add(val);
+        list.add(val);
+        map.put(val, list.size()-1);
         return true;
     }
     
     public boolean remove(int val) {
-        if (set.contains(val)) {
-            set.remove(val);
-            return true;
+        if (!map.containsKey(val)) {
+            return false;
         }
-        return false;
+
+        // Index of the element to remove
+        int index = map.get(val);
+
+        // Last element in the list
+        int lastElement = list.get(list.size() - 1);
+
+        // Move the last element to the removed element's position
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+
+        // Remove the last element
+        list.remove(list.size() - 1);
+
+        // Remove the value from the map
+        map.remove(val);
+
+        return true;
+        
     }
     
     public int getRandom() {
-        int index = random.nextInt(set.size());
+        int index = random.nextInt(list.size());
+        
 
-        int i = 0;
-        for (int num : set) {
-            if (i == index) {
-                return num;
-            }
-            i++;
-        }
-
-        return -1; // never reached if set is not empty
+        return list.get(index); // never reached if set is not empty
     }
 }
 
